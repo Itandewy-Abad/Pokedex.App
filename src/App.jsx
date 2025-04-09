37b0138ce79f8d9b0189f4a3fb16ccdace82696f
Itandewy-Abad <itanabad@gmail.com>
2025-04-09 16:57:40 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import LayoutHome from './pages/home/layout/layoutHome.jsx'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <LayoutHome />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/home/header/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import css from './header.module.scss'
+
+export default function header() {
+  return (
+    <div>header im here</div>
+  )
+}
