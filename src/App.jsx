8594d32b8907fcd868b4a0e849b5cec6f0811efc
Itandewy-Abad <itanabad@gmail.com>
2025-04-11 09:12:54 -0600

    new version

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,44 @@
-import { useState } from 'react'
-import LayoutHome from './pages/home/layout/layoutHome.jsx'
+import React, { useState, useEffect } from 'react';
 
-function App() {
-
+function PokemonCard({ pokemon }) {
   return (
-    <>
-      <div>
-        <LayoutHome />
-      </div>
-    </>
-  )
+    <div className="pokemon-card">
+      <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+      <h2>{pokemon.name}</h2>
+      <p>Clasificación: {pokemon.types[0].type.name}</p>
+      <p>Nivel de poderes: {pokemon.stats[0].base_stat}</p>
+    </div>
+  );
 }
 
-export default App
+function App() {
+  const [pokemones, setPokemones] = useState([]);
+
+  useEffect(() => {
+    const fetchPokemones = async () => {
+      const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=20');
+      const data = await response.json();
+      const pokemonesData = data.results.map(async (pokemon) => {
+        const response = await fetch(pokemon.url);
+        const data = await response.json();
+        return data;
+      });
+      const pokemonesResolved = await Promise.all(pokemonesData);
+      setPokemones(pokemonesResolved);
+    };
+    fetchPokemones();
+  }, []);
+
+  return (
+    <div className='title'>P Ó K E D E X <br></br>
+      <input type="search" name="" id="busqueda" placeholder='Buscar Pókemon...' />
+      <div className="pokedex">
+        {pokemones.map((pokemon) => (
+          <PokemonCard key={pokemon.id} pokemon={pokemon} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
--- src/pages/home/header/Header.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import css from './header.module.scss'
-
-export default function header() {
-  return (
-    <div>header im here</div>
-  )
-}
